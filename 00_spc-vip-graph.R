881248590616b96bc2bd944876f84c493d7c5ded
Philipp Baumann <philipp.baumann@usys.ethz.ch>
2017-12-12 11:49:17 +0100

    Add code for spectra and VIP figure for publication

---
+++ 00_spc-vip-graph.R
@@ -0,0 +1,6 @@
+################################################################################
+## Create a stacked graph of raw spectra, preprocessed spectra and 
+## variable importance in the projection (VIP) scores for 
+## the total C PLSR model, total N PLSR model and clay PLSR model
+################################################################################
+
